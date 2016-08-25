b71b976aab1a89bb352870b76b02d8b70a0b659b
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-08-25 15:13:36 -0500

    subiendo ejercicio

---
+++ app.js
@@ -0,0 +1,3 @@
+var numero1 = parseInt(prompt ("Ingrese su sueldo semanal"));
+var numero2 = parseInt(prompt ("Ingrese su pago por hora"));
+	window.alert("Su sueldo semanal es " + (numero1 * numero2));
